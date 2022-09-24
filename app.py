88f0c73a7c018157198136150f60355979d0e560
Aswan Ur Rehman <aswanurrehman@gmail.com>
2022-09-24 13:48:46 +0530

    This commit includes updated app.py and ipyn file

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
+
     return render_template("home.html",prediction_text="The Person has {}".format(output))
 
 
